32bf843aa298aef306783a8dc4c2cb5fce1ce94d
Joelma Regina <joelma_silvaa@hotmail.com>
2024-10-15 16:34:54 +0100

    Upload more improvments

--- FirstAssignmentProgramming/src/firstassignmentprogramming/FirstAssignmentProgramming.java
+++ FirstAssignmentProgramming/src/firstassignmentprogramming/FirstAssignmentProgramming.java
@@ -30,42 +30,61 @@ public class FirstAssignmentProgramming {
         // Collects the movie name from the user
         movieName = myKB.nextLine();
         
-        
+        //Displays the number of characters of the film the user entered
         System.out.println("The number of characters of the name of your film is: " + movieName.length());
         
         if (!movieName.contains("Star")){
-            
-            System.out.println("Your film has " + "Star" + " on its name");
+            // Displays if the film starts with  the word "Star"
+            System.out.println("Your film name doesn't have \"Star\" on its name" );
         } else {
-            
-            System.out.println("Your film doesn't has " + "Star" + " on its name");
+            // Displays if the film doesn't starts with  the word "Star"
+            System.out.println("Your film has \"Star\" on its name");
         }
     
         //POSSIBLE IMPROVEMENT 1 -- Can you improve the decision below? If yes --> How?
         // Change the code to show your improvement, but make sure the functionality is EXACTLY THE SAME !
-        if (movieName.substring(0,1).equalsIgnoreCase("X")){
-            
-            System.out.println("That is probably an X-Men movie");
+        
+        switch (movieName.substring(0,1)){
+            case "X": case "x":
+                // Displays a message if the film name starts with "X"
+                System.out.println("That is probably an X-Men movie");
+                break;
+            case "Z" : case "z":
+                // Displays a message if the film name starts with "Z"
+                System.out.println("Interesting...is that a Zombie movie? Or Zoolander?");
+                break;
+            case "K" : case "k":
+                // Displays a message if the film name starts with "K"
+                System.out.println("I can't think of many movie's that start with a K");
+                break;
+            default:
+                // Displays a message if the film name doesn't starts with "X", "Z" nor "K" 
+                System.out.println("Your film name doesn't starts with \"X\", \"Z\" nor \"K\"");
+                break;
+        } 
+        
+        /*if (movieName.substring(0,1).equalsIgnoreCase("X")){
+
         } else if (movieName.substring(0,1).equalsIgnoreCase("Z")){
-            
+            // Displays a message if the film name starts with "Z"
             System.out.println("Interesting...is that a Zombie movie? Or Zoolander?");
-            
         } else if (movieName.substring(0,1).equalsIgnoreCase("K")){
-            
+            // Displays a message if the film name starts with "K"
             System.out.println("I can't think of many movie's that start with a K");
         } else {
-            
-            System.out.println("Output message 4");
-        }
+            // Displays a message if the film name doesn't starts with "X", "Z" nor "K" 
+            System.out.println("Your film doesnt film name doesn't starts with \"X\", \"Z\" nor \"K\"");
+        } */
         ///END OF POSSIBLE IMPROVEMENT 1
         
         try{
             System.out.println("How old are you?");
-            userAge = myKB.nextInt();
+            //Collects the age of the user
+            userAge = myKB.nextInt(); 
             
             //POSSIBLE IMPROVEMENT 2-- Can you improve the decision below? If yes --> How?
         // Change the code to show your improvement, but make sure the functionality is EXACTLY THE SAME !
-            if (userAge >= 50){
+            if ((userAge >= 50) && ( userAge <= 120 )){
                 
                 System.out.println("You don't look that old");
                 
