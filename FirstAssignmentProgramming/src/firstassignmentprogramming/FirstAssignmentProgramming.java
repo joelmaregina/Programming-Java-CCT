20ec176af1df5e0ab342a5a62b383d1ebdfc1c22
Joelma Regina <joelma_silvaa@hotmail.com>
2024-10-15 15:45:43 +0100

    Upload the start of the resolution of the first CA

--- FirstAssignmentProgramming/src/firstassignmentprogramming/FirstAssignmentProgramming.java
+++ FirstAssignmentProgramming/src/firstassignmentprogramming/FirstAssignmentProgramming.java
@@ -2,18 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package firstcaoriginalcode;
+package firstassignmentprogramming;
+
+import java.util.Scanner;
 
 /**
  *
  * @author joelm
  */
-public class FirstCAOriginalCode {
+public class FirstAssignmentProgramming {
 
     /**
      * @param args the command line arguments
      */
-    /* YOU HAVE TO 
+     /* YOU HAVE TO 
      * - Insert comments to explain what is happening
      * - REPLACE ALL THE 'Output Messages' with a clear and sensible message for the user
     */
@@ -25,19 +27,18 @@ public class FirstCAOriginalCode {
         
         System.out.println("Please enter the name of your favourite movie");
         
+        // Collects the movie name from the user
         movieName = myKB.nextLine();
         
         
-        System.out.println("Output message 1: " + movieName.length());
+        System.out.println("The number of characters of the name of your film is: " + movieName.length());
         
         if (!movieName.contains("Star")){
             
-            System.out.println("Output message 2");
+            System.out.println("Your film has " + "Star" + " on its name");
+        } else {
             
-        }
-        else
-        {
-            System.out.println("Output message 3");
+            System.out.println("Your film doesn't has " + "Star" + " on its name");
         }
     
         //POSSIBLE IMPROVEMENT 1 -- Can you improve the decision below? If yes --> How?
@@ -45,16 +46,14 @@ public class FirstCAOriginalCode {
         if (movieName.substring(0,1).equalsIgnoreCase("X")){
             
             System.out.println("That is probably an X-Men movie");
-        }
-        else if (movieName.substring(0,1).equalsIgnoreCase("Z")){
+        } else if (movieName.substring(0,1).equalsIgnoreCase("Z")){
             
             System.out.println("Interesting...is that a Zombie movie? Or Zoolander?");
             
-        }else if (movieName.substring(0,1).equalsIgnoreCase("K")){
+        } else if (movieName.substring(0,1).equalsIgnoreCase("K")){
             
             System.out.println("I can't think of many movie's that start with a K");
-        }
-        else {
+        } else {
             
             System.out.println("Output message 4");
         }
@@ -70,17 +69,14 @@ public class FirstCAOriginalCode {
                 
                 System.out.println("You don't look that old");
                 
-            }
-            else if ((userAge <50) && (userAge < 120)){
+            } else if ((userAge <50) && (userAge < 120)){
                 
                 System.out.println("Getting younger?");
                 
-            }
-            else if (userAge >= 120){
+            } else if (userAge >= 120){
                 
                  System.out.println("That's a great age!");
-            }
-            else {
+            } else {
                 
                 System.out.println("That's a great age!");
                 
